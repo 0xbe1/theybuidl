97ccc34fad36afc5176fc2bd19f3b64aca189253
0xbe1 <0xbetrue@gmail.com>
2022-05-14 23:12:31 +0800

    move github related code back to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
-import { fetchBuidlers } from './github'
+import { Octokit } from '@octokit/core'
+import { throttling } from '@octokit/plugin-throttling'
+
 
 const BOT_GITHUB_LOGINS = [
   'dependabot-preview[bot]',
@@ -49,7 +51,7 @@ const GITHUB_REPOS = [
   'ipfs/go-ipfs',
 ]
 
-export type Result<T> =
+type Result<T> =
   | {
       data: T
       error?: never
@@ -59,7 +61,7 @@ export type Result<T> =
       error: { message: string }
     }
 
-export type Buidler = {
+type Buidler = {
   login: string
   id: number
   url: string
@@ -349,3 +351,131 @@ function shouldRender(buidler: Buidler): boolean {
 }
 
 export default Home
+
+//
+// GitHub API Integration
+//
+
+type GitHubRepoContributor = {
+  login: string
+  id: number
+  url: string
+  contributions: number
+}
+
+type GitHubUser = {
+  login: string
+  name: string
+  company: string | null
+  blog: string | null
+  email: string | null
+  bio: string | null
+  twitter_username: string | null
+}
+
+const ThrottledOctokit = Octokit.plugin(throttling)
+const throttlingOptions = {
+  auth: process.env.GITHUB_ACCESS_TOKEN || '',
+  throttle: {
+    onRateLimit: (
+      retryAfter: number,
+      options: Record<string, any>,
+      octokit: Octokit
+    ) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      )
+
+      if (options.request.retryCount === 0) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+        return true
+      }
+    },
+    onSecondaryRateLimit: (
+      retryAfter: number,
+      options: Record<string, any>,
+      octokit: Octokit
+    ) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+      )
+    },
+  },
+}
+const octokit = new ThrottledOctokit(throttlingOptions)
+
+async function fetchBuidlers(repo: string): Promise<Result<Buidler[]>> {
+  try {
+    const tryRepoContributors = await fetchRepoContributors(repo)
+    if (tryRepoContributors.error) {
+      return { error: tryRepoContributors.error }
+    }
+    const tryUsers = await Promise.all(
+      tryRepoContributors.data.map((contributor) =>
+        fetchUser(contributor.login)
+      )
+    )
+    let result: Buidler[] = []
+    for (let i = 0; i < tryUsers.length; i++) {
+      const tryUser = tryUsers[i]
+      if (tryUser.error) {
+        return { error: tryUser.error }
+      }
+      const contributor = tryRepoContributors.data[i]
+      const user = tryUser.data
+      const buidler: Buidler = {
+        login: contributor.login,
+        id: contributor.id,
+        url: contributor.url,
+        repo: repo,
+        contributions: contributor.contributions,
+        name: user.name,
+        company: user.company,
+        blog: user.blog,
+        email: user.email,
+        bio: user.bio,
+        twitter_username: user.twitter_username,
+      }
+      result.push(buidler)
+    }
+    return { data: result }
+  } catch (error: any) {
+    return { error: { message: error.message } }
+  }
+}
+
+async function fetchRepoContributors(
+  repo: string
+): Promise<Result<GitHubRepoContributor[]>> {
+  const [owner, repoName] = repo.split('/')
+  try {
+    const repoContributorsResp = await octokit.request(
+      'GET /repos/{owner}/{repo}/contributors',
+      {
+        owner,
+        repo: repoName,
+      }
+    )
+    const repoContributors =
+      repoContributorsResp.data as GitHubRepoContributor[]
+    return { data: repoContributors }
+  } catch (error: any) {
+    console.log('fetchRepoContributors: ', error)
+    return { error: { message: error.message } }
+  }
+}
+
+async function fetchUser(username: string): Promise<Result<GitHubUser>> {
+  try {
+    const userResp = await octokit.request('GET /users/{username}', {
+      username,
+    })
+    const user = userResp.data as GitHubUser
+    return { data: { ...user } }
+  } catch (error: any) {
+    console.log('fetchUser: ', error)
+    return { error: { message: error.message } }
+  }
+}
